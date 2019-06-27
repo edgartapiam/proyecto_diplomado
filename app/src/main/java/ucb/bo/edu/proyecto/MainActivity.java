c2fb8f03bbbcfdbbb5f87db4da3d1d45d9b49a28
Edgar <ertapiam@gmail.com>
2019-06-27 00:05:51 -0400

    proyecto android

---
+++ app/src/main/java/ucb/bo/edu/proyecto/MainActivity.java
@@ -0,0 +1,13 @@
+package ucb.bo.edu.proyecto;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
